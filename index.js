cbdf2c95036d59abb15ba6e7feae5f9077238a63
Ade Indriawan <aindriawan1@gmail.com>
2017-09-13 01:30:05 +0700

    fixed typo on header setting

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 
 // Make the server CORS-ENABLE
 app.use((req, res, next) => {
-  res.header('Access-Control-Allor-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
