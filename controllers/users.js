2a507c3dd74fa424c3080cf7e3f1e1db6f883b0e
Ade Indriawan <aindriawan1@gmail.com>
2017-09-19 01:58:30 +0700

    fixed response object on /users API

--- controllers/users.js
+++ controllers/users.js
@@ -2,18 +2,20 @@ import User from '../models/user'
 import mongoose from 'mongoose'
 
 export const getAllUsers = (req, res) => {
-    let data = {}
+    let data = { user: [], }
     User.find({},  (err, user) => {
         user.map((item) => {
-            data['id'] = item._id,
-            data['username'] = item.username,
-            data['password'] = item.password,
-            data['email'] = item.email,
-            data['first_name'] = item.first_name,
-            data['last_name'] = item.last_name,
-            data['date_of_birth'] = item.date_of_birth.getDate() + '-' + item.date_of_birth.getMonth()+1 + '-' + item.date_of_birth.getFullYear(),
-            data['gender'] = item.gender,
-            data['role'] = item.role
+            data.user.push({
+                'id': item._id,
+                'username': item.username,
+                'password': item.password,
+                'email': item.email,
+                'first_name': item.first_name,
+                'last_name': item.last_name,
+                'data_of_birth': item.date_of_birth,
+                'gender': item.gender,
+                'role': item.role
+            })
         })
         res.json(data)
     })
--- dist/controllers/users.js
+++ dist/controllers/users.js
@@ -16,10 +16,20 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var getAllUsers = exports.getAllUsers = function getAllUsers(req, res) {
-    var data = {};
+    var data = { user: [] };
     _user2.default.find({}, function (err, user) {
         user.map(function (item) {
-            data['id'] = item._id, data['username'] = item.username, data['password'] = item.password, data['email'] = item.email, data['first_name'] = item.first_name, data['last_name'] = item.last_name, data['date_of_birth'] = item.date_of_birth.getDate() + '-' + item.date_of_birth.getMonth() + 1 + '-' + item.date_of_birth.getFullYear(), data['gender'] = item.gender, data['role'] = item.role;
+            data.user.push({
+                'id': item._id,
+                'username': item.username,
+                'password': item.password,
+                'email': item.email,
+                'first_name': item.first_name,
+                'last_name': item.last_name,
+                'data_of_birth': item.date_of_birth,
+                'gender': item.gender,
+                'role': item.role
+            });
         });
         res.json(data);
     });
@@ -30,17 +40,7 @@ var getUserById = exports.getUserById = function getUserById(req, res) {
     var data = {};
     _user2.default.findOne({ _id: id }, function (err, user) {
         user.map(function (item) {
-            data.push({
-                'id': item._id,
-                'username': item.username,
-                'password': item.password,
-                'email': item.email,
-                'first_name': item.first_name,
-                'last_name': item.last_name,
-                'gender': item.gender,
-                'role': item.role,
-                'date_of_birth': item.date_of_birth.getDate() + '-' + item.date_of_birth.getMonth() + 1 + '-' + item.date_of_birth.getFullYear()
-            });
+            data['id'] = item._id, data['username'] = item.username, data['password'] = item.password, data['email'] = item.email, data['first_name'] = item.first_name, data['last_name'] = item.last_name, data['date_of_birth'] = item.date_of_birth.getDate() + '-' + item.date_of_birth.getMonth() + 1 + '-' + item.date_of_birth.getFullYear(), data['gender'] = item.gender, data['role'] = item.role;
         });
         res.json(data);
     });
